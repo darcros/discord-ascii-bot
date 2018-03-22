b93720f70d48983ae632ca5feaaeb3002534ee3b
7ixi0 <darcros7@gmail.com>
2018-03-22 18:35:48 +0100

    fixed transparency support

--- functions/imageStreamToAscii.js
+++ functions/imageStreamToAscii.js
@@ -11,42 +11,46 @@ function imageStreamToAscii(stream, width, heigth, cusotmChars) {
     // create image processing pipeline with sharp
     // FIXME: create a pipeline and the use .clone() instead of creating a new instance every time
     const transformer = sharp()
-      // TODO: find better way, the real limit is not the width and heigth but total size
-      // TODO: scale values to compensate for characters being more tall than wide
       .resize(width, heigth)
-      // aspect ratio is ignored because the size passed in should be right
+      // ignore the aspect ratio because the size passed in should be right
       .ignoreAspectRatio()
       // make sure to use the right colorspace
-      // .toColorspace('srgb')
+      .toColorspace('srgb')
+      // set white backround for next action
+      .background({
+        r: 255,
+        g: 255,
+        b: 255,
+        alpha: 1
+      })
+      // merge with background to remove alpha channel
+      .flatten()
       .raw();
 
-    // TODO: write shorter
-    transformer.on('error', (err) => {
-      reject(err);
-    });
+    transformer.on('error', reject);
 
-    transformer.on('info', (info) => {
-      const { channels } = info;
-
-      transformer.on('data', (chunk) => {
-        // +=3 because there are 3 channels (red, green, blue)
-        for (let i = 0; i < chunk.length; i += channels) {
-          // TODO: test custom charset
-          const a = channels === 4 ? chunk[1 + 3] : false;
-          console.log(a);
-          const char = getChar(chunk[i], chunk[i + 1], chunk[i + 2], a, false || cusotmChars);
-          ASCIIString += char;
-        }
-      });
+    transformer.on('data', (chunk) => {
+      // +=3 because there are 3 channels (red, green, blue)
+      for (let i = 0; i < chunk.length; i += 3) {
+        // TODO: test custom charset
+        // TODO: before having all these variables it looked faster, do bechmark
+        const r = chunk[i];
+        const g = chunk[i + 1];
+        const b = chunk[i + 2];
+        // const a = channels === 4 ? chunk[i + 3] : false;
+        const char = getChar(r, g, b, false || cusotmChars);
+        ASCIIString += char;
+      }
     });
 
     // FIXME: if the dimesions are passed in the args there is no need to wait the event
+    // TODO: merge this event listener to the other one
     transformer.on('info', (info) => {
       // FIXME: having event listeners inside other event listeners is not a good idea
-      // the stram could end before calling info but i'm not sure and i would cause an error anyway
+      // the stream could end before calling info but it would cause an error anyway
       transformer.on('end', () => {
         for (let i = info.width; i < ASCIIString.length; i += info.width + 1) {
-          // add new lines at the width
+          // add new lines at the specfied width
           // FIXME: splice is very expensive, maybe it's best to create a new string
           ASCIIString = `${ASCIIString.slice(0, i)}\n${ASCIIString.slice(i)}`;
         }
@@ -57,7 +61,8 @@ function imageStreamToAscii(stream, width, heigth, cusotmChars) {
 
     stream
       .pipe(transformer)
-      // FIXME: find better way
+      // consume the stream
+      // FIXME: the stream gets already consumed by transformer.on('data')
       .on('data', () => { });
   });
 }
--- functions/pixelToChar.js
+++ functions/pixelToChar.js
@@ -3,13 +3,9 @@ const { defaultPixels } = require('../config.json');
 
 // TODO: throw error on missing params
 // TODO: use default parameters for customPixels
-function getChar(r, g, b, a, customPixels) { // TODO: move to different file
+function getChar(r, g, b, customPixels) { // TODO: move to different file
   const pixels = customPixels || defaultPixels;
-  let brightness = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
-  if (a !== undefined &&
-    a !== null &&
-    a !== false &&
-    a !== 0) brightness *= a;
+  const brightness = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
 
   const index = Math.round((brightness / 255) * (pixels.length - 1));
   return pixels.charAt(index);
