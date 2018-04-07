98f52f6f15176ad4915b4ba3dd492b4cfa6d42df
7ixi0 <darcros7@gmail.com>
2018-04-07 17:05:51 +0200

    reminder

--- commands/text.js
+++ commands/text.js
@@ -3,6 +3,7 @@ const figlet = require('figlet');
 const fontName = require('../functions/text/fontName');
 const log = require('../functions/log');
 
+// TODO: pass custom kerning
 module.exports = (client, message, args) => {
   const font = fontName.in(args.f || 'standard');
   figlet(args._, { font }, (err, text) => {
