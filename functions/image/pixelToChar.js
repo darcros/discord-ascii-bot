fb6aecbd0cfc8315ad64a36355812ba3f76821a0
7ixi0 <darcros7@gmail.com>
2018-04-07 16:57:40 +0200

    now it throws error on missing params

--- functions/image/pixelToChar.js
+++ functions/image/pixelToChar.js
@@ -1,8 +1,11 @@
 // const defaultPixels = '%&#MHGw*+-. ';
 const { defaultPixels } = require('../../config.json');
 
-// TODO: throw error on missing params
 module.exports = (r, g, b, customPixels) => {
+  if (!r) throw new Error('r is required');
+  if (!g) throw new Error('g is required');
+  if (!b) throw new Error('b is required');
+
   const pixels = customPixels || defaultPixels;
   const brightness = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
 
