55f72261cd62cc956f5ecfef697d220b002c8c83
7ixi0 <darcros7@gmail.com>
2018-04-07 17:04:20 +0200

    fixed bug: if r, g or b were 0 it threw errpr

--- functions/image/pixelToChar.js
+++ functions/image/pixelToChar.js
@@ -3,9 +3,9 @@ const { defaultPixels } = require('../../config.json');
 
 module.exports = (r, g, b, customPixels) => {
   // TODO: decide if this is necessary or if it's it just bloating the function
-  if (!r) throw new Error('r is required');
-  if (!g) throw new Error('g is required');
-  if (!b) throw new Error('b is required');
+  if (r === undefined) throw new Error('r is required');
+  if (g === undefined) throw new Error('g is required');
+  if (b === undefined) throw new Error('b is required');
 
   const pixels = customPixels || defaultPixels;
   const brightness = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
