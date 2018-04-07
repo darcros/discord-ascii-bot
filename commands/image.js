3f5eac6d71adf06635022bad2afbb07c39647306
7ixi0 <darcros7@gmail.com>
2018-04-07 16:23:03 +0200

    Send messages on error

--- commands/image.js
+++ commands/image.js
@@ -1,26 +1,36 @@
 const urlImageToAscii = require('../functions/image/ImageUrlToAscii');
 
 module.exports = (client, message) => {
-  message.attachments.forEach(() => {
-    message.attachments.forEach((attachment) => {
-      // if the attachment is an image
-      // FIXME: check mime type and magic numbers to be sure it's an image
-      // event though magic numbers are probably already checked by sharp
+  let foundImage = false;
 
-      // TODO: send error if none of the attachments were images
-      if (attachment.width && attachment.height) {
-        if (attachment.filename.endsWith('.gif')) return; // avoid GIF files
+  message.attachments.forEach((attachment) => {
+    // if the attachment is an image
+    // FIXME: also check mime type to be sure it's an image
 
-        // convert image
-        // use 'proxyURL' instead of 'url' so the IP of the bot doesn't leak
-        urlImageToAscii(attachment.proxyURL, attachment.width, attachment.height)
-          .then((ascii) => {
-            // send result
-            message.channel.send(ascii, { code: true });
-          })
-          // TODO: inform user of the error
-          .catch(err => client.log('error', err));
+    // TODO: send error if none of the attachments were images
+    if (attachment.width && attachment.height) {
+      foundImage = true;
+
+      // avoid GIF files
+      if (attachment.filename.endsWith('.gif')) {
+        message.reply('GIFs are not supported.');
+        return;
       }
-    });
+
+      // convert image
+      // using 'proxyURL' instead of 'url' so the IP of the bot doesn't leak
+      urlImageToAscii(attachment.proxyURL, attachment.width, attachment.height)
+        .then((ascii) => {
+          // send result
+          message.channel.send(ascii, { code: true });
+        })
+        // TODO: inform user of the error
+        .catch((err) => {
+          message.reply('an unknown error occurred');
+          client.log('error', err);
+        });
+    }
   });
+
+  if (!foundImage) message.reply('you must attach an image to you message.');
 };
