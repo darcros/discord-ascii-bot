40ac93741d58f2906937f8df95321560be154375
7ixi0 <darcros7@gmail.com>
2018-04-10 23:08:54 +0200

    added search

--- commands/fontList.js
+++ commands/fontList.js
@@ -1,3 +1,4 @@
+const minimist = require('minimist');
 const figlet = require('figlet');
 
 const fontName = require('../functions/text/fontName');
@@ -5,11 +6,23 @@ const fontName = require('../functions/text/fontName');
 // preload the font list
 const list = figlet.fontsSync().map(fontName.out);
 
-module.exports = (client, message) => {
+const parser = argString => minimist(argString, {
+  alias: {
+    search: ['s', 'string', 'searchString', 'stringSearch']
+  },
+  default: {
+    search: ''
+  }
+});
+
+module.exports = (client, message, argString) => {
+  const args = parser(argString);
+
   if (message.channel.type !== 'dm') message.reply('sending the list in your DMs.');
 
   // prepare the message to send
   const msg = list
+    .filter(elem => elem.toLowerCase().includes(args.search.toLowerCase()))
     .reduce((prev, next) => `${prev}, ${next}`);
 
   // send with split enabled
