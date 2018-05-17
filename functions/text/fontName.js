8ea5259785de1b4948e5835ca188b54d0e6d7965
7ixi0 <darcros7@gmail.com>
2018-05-17 16:47:01 +0200

    fixed a bug where only first occurence was replaced

--- functions/text/fontName.js
+++ functions/text/fontName.js
@@ -1,9 +1,9 @@
 module.exports.in = (name) => {
   if (!name) return undefined;
-  return name.replace('_', ' ');
+  return name.replace(/_/g, ' ');
 };
 
 module.exports.out = (name) => {
   if (!name) return undefined;
-  return name.replace(' ', '_');
+  return name.replace(/ /g, '_');
 };
