57ef43b2fd3e7eb7d97e2e423fa424281920afb8
Dario Crosa <darcros7@gmail.com>
2019-01-03 21:51:12 +0100

    convert image to b&w with sharp instad of using JS function

--- functions/image/imageStreamToAscii.js
+++ functions/image/imageStreamToAscii.js
@@ -1,42 +1,31 @@
 const sharp = require('sharp');
-const getChar = require('./pixelToChar');
+
+// const defaultPixels = '%&#MHGw*+-. ';
+const { defaultPixels } = require('../../config');
 
 module.exports = (stream, width, height, customChars) => new Promise((resolve, reject) => {
   if (!stream) reject(new Error('stream is required'));
   if (!width) reject(new Error('width is required'));
   if (!height) reject(new Error('height is required'));
 
+  const pixels = customChars || defaultPixels;
   let ASCIIString = '';
 
-  // create image processing pipeline with sharp
   const transformer = sharp()
     .resize(width, height)
     // ignore the aspect ratio because the size passed in should be right
     .ignoreAspectRatio()
-    // make sure to use the right colorspace
-    .toColorspace('srgb')
-    // set white background for next action
-    .background({
-      r: 255,
-      g: 255,
-      b: 255,
-      alpha: 1
-    })
-    // merge with background to remove alpha channel
-    .flatten()
+    // convert image to black and white
+    .toColorspace('b-w')
     .raw();
 
   transformer.on('error', reject);
 
   transformer.on('data', (chunk) => {
-    // +=3 because there are 3 channels (red, green, blue)
-    for (let i = 0; i < chunk.length; i += 3) {
-      const r = chunk[i];
-      const g = chunk[i + 1];
-      const b = chunk[i + 2];
-      // const a = channels === 4 ? chunk[i + 3] : false;
-      const char = getChar(r, g, b, false || customChars);
-      ASCIIString += char;
+    for (let i = 0; i < chunk.length; i++) {
+      const brightness = chunk[i];
+      const index = Math.floor((brightness / 255) * (pixels.length - 1));
+      ASCIIString += pixels.charAt(index);
     }
   });
 
