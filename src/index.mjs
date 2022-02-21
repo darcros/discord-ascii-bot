431db9833d2a82b3c2c228ec253d1ef5cc8cfe75
darcros <darcros7@gmail.com>
2022-02-21 18:01:51 +0100

    added deprecation message for chat commands

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
 import { Client, Intents } from 'discord.js';
 import { dispath } from './interactionDispatcher.mjs';
+import { onMessage } from './handleChatEvent.mjs';
 
 // load env variables
 dotenv.config();
 const { TOKEN } = process.env;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    // TODO: remove after May 1, 2022 when the bot will no longer have access to message content
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
@@ -15,5 +22,9 @@ client.on('ready', () => {
 client.on('interactionCreate', async (interaction) => {
   await dispath(interaction);
 });
+
+// Respond to chat commands telling the user to use the slash commands instead
+// TODO: remove after May 1, 2022 when the bot will no longer have access to message content
+client.on('messageCreate', (msg) => onMessage(client, msg));
 
 client.login(TOKEN);
